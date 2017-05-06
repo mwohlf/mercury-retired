63082dbf6f2c5ec5706d6d58300b481fd74c4d36
michael <michael@wohlfart.net>
2017-05-06 14:20:57 +0200

    initial commit

---
+++ backend/src/main/java/net/wohlfart/MercuryApplication.java
@@ -0,0 +1,12 @@
+package net.wohlfart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercuryApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/net/wohlfart/PersonRepository.java
@@ -0,0 +1,12 @@
+package net.wohlfart;
+ 
+import java.util.List;
+ 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+ 
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository<Person, Long> {
+ 
+}
