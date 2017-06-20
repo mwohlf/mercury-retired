4f2c8d23b58aef218df103b86b3e0455a0a6d944
michael <michael@wohlfart.net>
2017-06-20 22:14:16 +0200

    bootstrap3 navbar

--- frontend/src/main/frontend/src/app/register/register.component.ts
+++ frontend/src/main/frontend/src/app/register/register.component.ts
@@ -2,7 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 
 // component in Angular is basically a controller class with a template
+import { UserService } from '../_service/user.service';
 import { AlertService } from '../_service/alert.service';
+import { User } from '../_model/user';
 
 @Component({
     // the name for the HTML class selector (<register></register>)
@@ -12,17 +14,41 @@ import { AlertService } from '../_service/alert.service';
     // link the stylesheet
     styleUrls: ['./register.component.css'],
     // services
-    providers: [AlertService]
+    providers: [AlertService, UserService]
 })
 
 export class RegisterComponent implements OnInit {
 
+    private newLogin;
+
+    private newEmail;
+
+    private newPassword;
+
     constructor(
+        private userService: UserService,
         private alertService: AlertService
-    ) {}
+    ) { }
 
     ngOnInit() {
-        this.alertService.success("<ngOnInit> for RegisterComponent" );
+        this.alertService.success("<ngOnInit> for RegisterComponent");
+    }
+
+    register() {
+        console.log("<register> newLogin: " + this.newLogin);
+        var user = new User();
+        user.login = this.newLogin;
+        user.email = this.newEmail;
+        this.userService
+            .create(user)
+            .subscribe(
+            result => {
+                // registering done
+            },
+            error => {
+                this.alertService.error(error);
+            }
+            );
     }
 
 }
