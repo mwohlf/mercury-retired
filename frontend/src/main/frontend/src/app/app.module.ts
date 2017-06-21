55c0b9f674824c760112e510d9d30bfd911451f1
Michael Wohlfart <mwohlfart@vwd.com>
2017-06-21 16:13:44 +0200

    bootstrap 4 minus the styles

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './app.routing';
@@ -29,7 +28,6 @@ import { RegisterComponent } from './register/register.component';
     FormsModule,
     HttpModule,
     RoutingModule,
-    NgbModule.forRoot(),
   ],
   providers: [],
   // loading on appstart
